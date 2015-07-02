f97cb544ec22097835d8ed57d53e2cf4a2edb4e7
Sridhar Mane <sridhars4s@gmail.com>
2015-07-02 17:44:57 -0400

    Adding Android platform files before implementing custom background service.

---
+++ platforms/android/src/com/push/plugin/App.java
@@ -0,0 +1,14 @@
+package com.push.plugin;
+
+        import android.app.Application;
+        import org.apache.cordova.core.ParsePlugin;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        ParsePlugin.initializeParseWithApplication(this);
+    }
+
+}
